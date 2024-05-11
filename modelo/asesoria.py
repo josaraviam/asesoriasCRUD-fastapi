de97494552525897f40644de77e0f5798a60f149
Joel Saravia <65045163+josaraviam@users.noreply.github.com>
2024-05-11 15:45:43 -0600

    o

--- modelo/asesoria.py
+++ modelo/asesoria.py
@@ -1,14 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
 
-
 class Asesoria(BaseModel):
-    id: Optional[str] = None
-    titulo: str
-    descripcion: str
-    fecha: datetime
-    hora: str
-    profesor: str
-    usuario_id: str # Se me ocurre que podemos mostrar este en pantalla para simplificar
-
+    id: Optional[str] = Field(None, description="Identificador único de la asesoría, generado automáticamente.")
+    materia: str = Field(..., description="Materia de la asesoría.")
+    tema: str = Field(..., description="Tema con el que necesitas ayuda.")
+    fecha: datetime = Field(..., description="Fecha en la que se llevará a cabo la asesoría.")
+    hora: str = Field(..., description="Hora de la asesoría, debe ser en formato HH:MM.")
+    profesor: str = Field(..., description="Nombre del profesor que impartirá la asesoría.")
+    usuario_id: str = Field(..., description="Identificador del usuario que solicita la asesoría.")
