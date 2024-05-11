502c108fdc69e97abd5bb802c963c33ec8050023
Joel Saravia <65045163+josaraviam@users.noreply.github.com>
2024-05-11 15:57:32 -0600

    o.o

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from db.db import client
 from controladores.asesoriaCRUD import router as asesorias_router
-from controladores.usuarioCRUD import router as usuarios_router  # Importa el router de usuarios
 
 app = FastAPI()
 app.include_router(asesorias_router, tags=["asesorias"], prefix="/asesorias")
-app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")  # Incluye el router de usuarios
 
 @app.get("/")
 async def read_root():
